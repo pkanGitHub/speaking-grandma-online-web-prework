41b10b402b50176719e2a2c84e74202bb8a869d4
Phyllis Kan <pyk9q7@gmail.com>
2019-05-06 23:10:10 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -6,7 +6,6 @@ def speak_to_grandma(phrase)
     return "HUH?! SPEAK UP, SONNY!"
   else
     return "NO, NOT SINCE 1938!"
-
 end
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
